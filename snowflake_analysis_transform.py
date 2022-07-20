934e0ef3e30d770ee1f8903a100fd0fb340b4f98
Connor Callen <86798098+connorcallen@users.noreply.github.com>
2022-07-20 12:19:47 -0600

    Update snowflake_analysis_transform.py

--- snowflake_analysis_transform.py
+++ snowflake_analysis_transform.py
@@ -6,8 +6,12 @@ from urllib.error import URLError
 
 streamlit.title('Data transformation for hemo values in snowflake')
 
+my_cur=my_cnx.cursor()
+df=my_cur.execute("select * from STITCH.STITCH2_INTEGRATION.GREYLEDGE_DATA_CLOUD__TREATMENT_HEMOANALYSIS_VALUES")
+df=streamlit.dataframe(df)
+print(df)
 
-with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from STITCH.STITCH2_INTEGRATION.GREYLEDGE_DATA_CLOUD__TREATMENT_HEMOANALYSIS_VALUES")
-    return my_cur.fetchall()
+#with my_cnx.cursor() as my_cur:
+ #   my_cur.execute("select * from STITCH.STITCH2_INTEGRATION.GREYLEDGE_DATA_CLOUD__TREATMENT_HEMOANALYSIS_VALUES")
+  #  return my_cur.fetchall()
 
