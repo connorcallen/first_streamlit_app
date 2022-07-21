61e6bc31f0479a1f3bdb15717f1b694b398046a6
Connor Callen <86798098+connorcallen@users.noreply.github.com>
2022-07-21 12:33:33 -0600

    Update snowflake_analysis_transform.py

--- snowflake_analysis_transform.py
+++ snowflake_analysis_transform.py
@@ -1,11 +1,11 @@
-import streamlit
+import streamlit as st
 import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('Data transformation for hemo values in snowflake')
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+st.title('Data transformation for hemo values in snowflake')
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 table_list = my_cur.execute("select IDPATIENTTREATMENT,\
                         ANALYSIS_TYPE,\
@@ -14,15 +14,15 @@ table_list = my_cur.execute("select IDPATIENTTREATMENT,\
                         TREATMENT_VOLUME_HARVESTED_ML,\
                         TOTAL_VOLUME_DELIVERED_FROM_INJECTION_SITES_ML,\
                         RECORD_OWNER from STITCH.STITCH2_INTEGRATION.GREYLEDGE_DATA_CLOUD__TREATMENT_HEMOANALYSIS_VALUES;").fetchall()
-df=streamlit.dataframe(table_list)
-df.rename(columns={0:'id_patient_treatment',
-                   1:'analysis_type',
-                   2:'Pre',
-                   3:'Post',
-                   4:'volume_harvested',
-                   5:'volume_injected',
-                   6:'record_owner'},inplace=True)
-streamlit.text(df)
+df=st.dataframe(table_list)
+#df.rename(columns={0:'id_patient_treatment',
+ #                  1:'analysis_type',
+  #                 2:'Pre',
+   #                3:'Post',
+    #               4:'volume_harvested',
+     #              5:'volume_injected',
+      #             6:'record_owner'},inplace=True)
+streamlit.text(df.dtypes)
 #analysis_data = my_cur.fetchall()
 #df=streamlit.dataframe(analysis_data)
 #streamlit.text(df.columns)
