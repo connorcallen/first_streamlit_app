d597106f716f33ee19599156ba72c2f1b66bad27
Connor Callen <86798098+connorcallen@users.noreply.github.com>
2022-07-21 12:27:01 -0600

    Update snowflake_analysis_transform.py

--- snowflake_analysis_transform.py
+++ snowflake_analysis_transform.py
@@ -7,9 +7,25 @@ from urllib.error import URLError
 streamlit.title('Data transformation for hemo values in snowflake')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from GREYLEDGE_DATA_CLOUD__TREATMENT_HEMOANALYSIS_VALUES")
-analysis_data = my_cur.fetchall()
-df=streamlit.dataframe(analysis_data)
-streamlit.text(df.columns)
-streamlit.text(df.columns())
+table_list = my_cur.execute("select IDPATIENTTREATMENT,\
+                        ANALYSIS_TYPE,\
+                        PRE_VALUE,\
+                        POST_VALUE,\
+                        TREATMENT_VOLUME_HARVESTED_ML,\
+                        TOTAL_VOLUME_DELIVERED_FROM_INJECTION_SITES_ML,\
+                        RECORD_OWNER from STITCH.STITCH2_INTEGRATION.GREYLEDGE_DATA_CLOUD__TREATMENT_HEMOANALYSIS_VALUES;").fetchall()
+df=pd.DataFrame(table_list)
+df.rename(columns={0:'id_patient_treatment',
+                   1:'analysis_type',
+                   2:'Pre',
+                   3:'Post',
+                   4:'volume_harvested',
+                   5:'volume_injected',
+                   6:'record_owner'},inplace=True)
+streamlit.dataframe(df)
+
+#analysis_data = my_cur.fetchall()
+#df=streamlit.dataframe(analysis_data)
+#streamlit.text(df.columns)
+#streamlit.text(df.columns())
  
